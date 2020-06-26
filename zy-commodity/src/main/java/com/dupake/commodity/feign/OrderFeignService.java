384d7daeda16c676b5809073863c0d71de400ad2
xt <hb_cxtl@163.com>
2020-06-26 11:59:31 +0800

    解决异常

--- zy-commodity/src/main/java/com/dupake/commodity/feign/OrderFeignService.java
+++ zy-commodity/src/main/java/com/dupake/commodity/feign/OrderFeignService.java
@@ -1,6 +1,7 @@
 package com.dupake.commodity.feign;
 
 import com.dupake.commodity.feign.hystrix.HystrixClientFallback;
+import com.dupake.common.message.CommonResult;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,5 +15,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 public interface OrderFeignService {
 
     @GetMapping("/test/hello")
-    Result hello();
+    CommonResult hello();
 }
--- zy-order/src/main/java/com/dupake/order/controller/OrderController.java
+++ zy-order/src/main/java/com/dupake/order/controller/OrderController.java
@@ -1,13 +1,15 @@
 package com.dupake.order.controller;
 
+import com.dupake.common.message.CommonResult;
 import com.dupake.order.entity.Order;
 import com.dupake.order.service.OrderService;
 import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * @author dupake
@@ -21,44 +23,44 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class OrderController {
 
-    @Autowired
+    @Resource
     private OrderService orderService;
 
 
     @ApiOperation(value = "查询订单信息", notes = "查询订单信息 - for-web")
     @GetMapping(value = "/findInfoById")
-    public Result findInfoById(@RequestParam(value = "id") Integer id) {
+    public CommonResult findInfoById(@RequestParam(value = "id") Integer id) {
         Order authUser = orderService.findInfoById(id);
-        return Result.ok(authUser);
+        return CommonResult.success(authUser);
     }
 
     @ApiOperation(value = "查询订单列表", notes = "查询订单列表 - for-web")
     @GetMapping(value = "/findList")
-    public Result findList(@RequestParam(value = "pageNum") Integer pageNum,
+    public CommonResult findList(@RequestParam(value = "pageNum") Integer pageNum,
                            @RequestParam(value = "pageSize") Integer pageSize) {
         PageInfo<Order> list = orderService.findList(pageNum, pageSize);
-        return Result.ok(list);
+        return CommonResult.success(list);
     }
 
     @ApiOperation(value = "新增订单", notes = "新增订单 - for-web")
     @PostMapping(value = "/insert")
-    public Result insert(@RequestBody Order user) {
+    public CommonResult insert(@RequestBody Order user) {
         Order authUser = orderService.insert(user);
-        return Result.ok(authUser);
+        return CommonResult.success(authUser);
     }
 
     @ApiOperation(value = "修改订单", notes = "修改订单 - for-web")
     @PostMapping(value = "/update")
-    public Result update(@RequestBody Order user) {
+    public CommonResult update(@RequestBody Order user) {
         Order authUser = orderService.update(user);
-        return Result.ok(authUser);
+        return CommonResult.success(authUser);
     }
 
     @ApiOperation(value = "删除订单", notes = "删除订单 - for-web")
     @PostMapping(value = "/delete")
-    public Result deleteByIds(@RequestParam Integer id) {
+    public CommonResult deleteByIds(@RequestParam Integer id) {
         orderService.deleteByIds(id);
-        return Result.ok();
+        return CommonResult.success();
     }
 
 }
