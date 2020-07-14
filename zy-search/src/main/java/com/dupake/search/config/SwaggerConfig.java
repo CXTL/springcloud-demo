282506c63f5d340fbbf533b3784d6950a1020141
陈啸天 <hb_cxtl@163.com>
2020-07-14 17:37:30 +0800

    es 模块修改

--- zy-search/src/main/java/com/dupake/search/config/SwaggerConfig.java
+++ zy-search/src/main/java/com/dupake/search/config/SwaggerConfig.java
@@ -3,12 +3,18 @@ package com.dupake.search.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -19,15 +25,28 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.dupake.hmorder.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.dupake.search.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(defaultHeader())
+                ;
+    }
+    private static List<Parameter> defaultHeader(){
+        ParameterBuilder appToken = new ParameterBuilder();
+        appToken.name("Authorization").description("登录令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        ParameterBuilder appSubmitTolken = new ParameterBuilder();
+        appSubmitTolken.name("SubmitToken").description("重复提交令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+
+        List<Parameter> pars = new ArrayList<>();
+        pars.add(appToken.build());
+        pars.add(appSubmitTolken.build());
+        return pars;
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("XT ORDER APIS")
-                .description("订单服务")
+                .description("查询服务")
                 .version("1.0")
                 .build();
     }
--- zy-search/src/main/java/com/dupake/search/controller/EsController.java
+++ zy-search/src/main/java/com/dupake/search/controller/EsController.java
@@ -1,54 +1,90 @@
-//package com.dupake.search.controller;
-//
-//import com.alibaba.fastjson.JSONObject;
-//import com.dupake.common.message.Result;
-//import io.swagger.annotations.ApiOperation;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.*;
-//
-///**
-// * @author dupake
-// * @version 1.0.0
-// * @ClassName EsController.java
-// * @Description TODO
-// * @createTime 2020年05月02日 15:14:00
-// */
-//@Slf4j
-//public class EsController {
-//
-//
-//    private
-//
-//
-//
-//
-//    /**
-//     * @Description 创建Elastic索引
-//     * @param idxVo
-//     * @return xyz.wongs.weathertop.base.message.response.ResponseResult
-//     * @throws
-//     * @date 2019/11/19 11:07
-//     */
-//    @RequestMapping(value = "/createIndex",method = RequestMethod.POST)
-//    public Result createIndex(@RequestBody IdxVo idxVo){
-//
-//        try {
-//            //索引不存在，再创建，否则不允许创建
-//            if(!baseElasticDao.indexExist(idxVo.getIdxName())){
-//                String idxSql = JSONObject.toJSONString(idxVo.getIdxSql());
-//                log.warn(" idxName={}, idxSql={}",idxVo.getIdxName(),idxSql);
-//                baseElasticDao.createIndex(idxVo.getIdxName(),idxSql);
-//            } else{
-//                response.setStatus(false);
-//                response.setCode(ResponseCode.DUPLICATEKEY_ERROR_CODE.getCode());
-//                response.setMsg("索引已经存在，不允许创建");
-//            }
-//        } catch (Exception e) {
-//            response.setStatus(false);
-//            response.setCode(ResponseCode.ERROR.getCode());
-//            response.setMsg(ResponseCode.ERROR.getMsg());
-//        }
-//        return response;
-//    }
-//
-//}
+package com.dupake.search.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.dupake.search.domain.Person;
+import com.dupake.search.domain.Response;
+import com.dupake.search.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author zhaoliancan
+ * @version 1.0.0
+ * @ClassName EsController.java
+ * @Description TODO
+ * @createTime 2019年09月12日 09:26:00
+ */
+@RestController
+@RequestMapping("/es")
+public class EsController {
+
+
+    @Autowired
+    PersonService personService;
+
+
+    /**
+     * 向es添加数据
+     */
+    @PostMapping("/insert")
+    public Object insert(){
+        JSONObject object=null;
+        try {
+            Person p1 =new Person("123456","zlc","西湖区",18,"男","hangzhou");
+          Person savePerson=  personService.save(p1);
+          object= (JSONObject) JSON.toJSON(savePerson);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return object;
+    }
+
+
+    /**
+     * 从es删除数据
+     * @param person
+     * @return
+     */
+    @DeleteMapping("/delete")
+    public Object delete(@RequestBody Person person){
+
+        try {
+            personService.delete(person);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.failed404();
+        }
+        return Response.successMsg();
+
+    }
+
+
+    /**
+     * 查询操作
+     * @param pageNo
+     * @param pageSize 查询大小
+     * @param key
+     * @return
+     */
+    @GetMapping("/query")
+    public Object query(@RequestParam(value = "pageNo",required = false)Integer pageNo, @RequestParam(value = "pageSize",required = false)Integer pageSize,
+                        @RequestParam(value = "key",required = false)String key){
+
+        Response response=new Response();
+        try {
+            Page<Person> personPage= personService.pageQuery(pageNo,pageSize,key);
+            response.setData(personPage);
+            response.setMessage("请求成功");
+            response.setCode(200);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.failed404();
+        }
+        return response;
+
+    }
+}
