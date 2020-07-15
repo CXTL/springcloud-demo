987060b9665645a2e2c3931c750fd50ba4e954e7
陈啸天 <hb_cxtl@163.com>
2020-07-15 16:57:57 +0800

    es7.1.1 初步集成 成功

---
+++ zy-search/src/main/java/com/dupake/search/dto/ReportDataDTO.java
@@ -0,0 +1,29 @@
+package com.dupake.search.dto;
+
+/**
+ * ReportDataDTO
+ *
+ * @author star
+ */
+public class ReportDataDTO {
+
+    private long timestamp;
+
+    private long views;
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public long getViews() {
+        return views;
+    }
+
+    public void setViews(long views) {
+        this.views = views;
+    }
+}
