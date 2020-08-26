7d6699528f3a4d82340f3e4a63b7e2cab30e4bea
陈啸天 <hb_cxtl@163.com>
2020-08-26 17:29:08 +0800

    解决服务跨域问题

---
+++ baby-finance/src/main/java/com/dupake/finance/config/CorsConfig.java
@@ -0,0 +1,41 @@
+package com.dupake.finance.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @ClassName CorsConfig
+ * @Description 跨域配置
+ * @Author dupake
+ * @Date 2020/6/22 10:01
+ */
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        //允许所有域名进行跨域调用
+        config.addAllowedOrigin("*");
+        //允许跨越发送cookie
+        config.setAllowCredentials(true);
+        //放行全部原始头信息
+        config.addAllowedHeader("*");
+        //允许所有请求方法跨域调用
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new CorsFilter(source));
+        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return new CorsFilter(source);
+    }
+
+
+
+}
